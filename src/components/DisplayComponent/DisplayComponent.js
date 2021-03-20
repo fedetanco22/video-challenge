88c56728af84e0c9913a1bf74637f637408ed9ad
fede tanco <63819426+fedetanco22@users.noreply.github.com>
2021-03-20 18:41:36 -0300

    commit to pull

--- src/components/DisplayComponent/DisplayComponent.js
+++ src/components/DisplayComponent/DisplayComponent.js
@@ -1,16 +1,11 @@
 import { useState, useEffect } from "react";
-
-import axios from "axios";
+import useAppContext from "../../context/useAppContext";
 
 export default function DisplayComponent() {
-	const [videos, setVideos] = useState({});
-	console.log(videos);
+	const { getVideos } = useAppContext();
 
 	useEffect(() => {
-		const url = "https://run.mocky.io/v3/93018145-28fc-4c92-a347-0118720d3ccd";
-		axios.get(url).then((response) => {
-			setVideos(response.data.categories[0].videos);
-		});
+		getVideos();
 	}, []);
 
 	return <h1>Hola Dislpay</h1>;
--- src/context/useAppContext.js
+++ src/context/useAppContext.js
@@ -1,10 +1,35 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
 
 const AppContext = createContext();
 const useAppContext = () => useContext(AppContext);
 
 export const AppProvider = ({ children }) => {
-	return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+	const initialState = [
+		{
+			videos: [],
+			selectedVideo: null,
+		},
+	];
+	const [state, setstate] = useState(initialState);
+
+	const getVideos = async () => {
+		const url = "https://run.mocky.io/v3/93018145-28fc-4c92-a347-0118720d3ccd";
+		await axios.get(url).then((response) => {
+			console.log(response.data.categories[0].videos);
+		});
+	};
+
+	// const getVideo = (id) => {
+	// 	const url = "https://run.mocky.io/v3/93018145-28fc-4c92-a347-0118720d3ccd";
+	// 	await axios.get(url).then((response) => {
+	// 		console.log(response);
+	// 	});
+	// };
+
+	return (
+		<AppContext.Provider value={{ getVideos }}>{children}</AppContext.Provider>
+	);
 };
 
 export default useAppContext;
